0a53c747c9b954ef7708bf943c2c056b161d3f6b
Sania majid <108267418+saniakhan234@users.noreply.github.com>
2023-07-20 02:41:19 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from car import Car 
+
+car_1 = Car("chevy","Corvette",2021,"Black")
+car_2 = Car("Ford","Mustang",2022,"Red") 
+
+print(car_1.make)
+print(car_1.model)
+print(car_1.year)
+print(car_1.color)
+
+car_1 .drive()
+car_2 .stop()
+
